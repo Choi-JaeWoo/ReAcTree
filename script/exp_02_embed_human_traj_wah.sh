a962244a3d96bd897bcdf00b0b7fe3889d06d752
etri <Choi-JaeWoo>
2025-10-21 15:09:35 +0900

    update script

--- script/exp_02_embed_human_traj_wah.sh
+++ script/exp_02_embed_human_traj_wah.sh
@@ -9,10 +9,3 @@
 # python src/embed_em.py --config-name=wah_reactree exp_type=embed_em llm_agent.working_memory=False dataset.embedding_root_dir='resource/wah/collect_human' dataset.em_root_dir='resource/wah/em_human'
 ### 1.4 ReAcTree+WM
 python src/embed_em.py --config-name=wah_reactree exp_type=embed_em llm_agent.working_memory=True dataset.embedding_root_dir='resource/wah/collect_human' dataset.em_root_dir='resource/wah/em_human'
-
-
-### 2. ALFRED 
-# ### 2.1 ReAct+WM
-# python src/embed_em.py --config-name=alfred_react llm_agent.working_memory=True dataset.embedding_root_dir='resource/alfred/collect_human' dataset.em_root_dir='resource/alfred/em_human'
-# ### 2.2 ReAcTree+WM
-# python src/embed_em.py --config-name=alfred_reactree llm_agent.working_memory=True dataset.embedding_root_dir='resource/alfred/collect_human' dataset.em_root_dir='resource/alfred/em_human'
--- script/exp_04_embed_llm_traj_wah.sh
+++ script/exp_04_embed_llm_traj_wah.sh
@@ -9,9 +9,3 @@
 # python src/embed_em.py --config-name=wah_reactree exp_type=embed_em llm_agent.working_memory=False dataset.embedding_root_dir=resource/wah/collect_llm dataset.em_root_dir=resource/wah/em_llm
 ### 1.4 ReAcTree+WM
 python src/embed_em.py --config-name=wah_reactree exp_type=embed_em llm_agent.working_memory=True dataset.embedding_root_dir=resource/wah/collect_llm dataset.em_root_dir=resource/wah/em_llm
-
-### 2. ALFRED 
-# ### 2.1 ReAct+WM
-# python src/embed_em.py --config-name=alfred_react dataset.check_success=True llm_agent.working_memory=True dataset.embedding_root_dir='resource/alfred/collect_llm' dataset.em_root_dir='resource/alfred/em_llm' 
-# ### 2.2 ReAcTree+WM
-# python src/embed_em.py --config-name=alfred_reactree dataset.check_success=True llm_agent.working_memory=True dataset.embedding_root_dir='resource/alfred/collect_llm' dataset.em_root_dir='resource/alfred/em_llm'
